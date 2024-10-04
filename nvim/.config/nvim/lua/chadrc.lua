1e0eb2cbb94073e17dd9cb24a807497f5be3ff2e
Josep de Cid <josep.de.cid@gmail.com>
2024-10-04 15:12:54 +0200

    Change nvim theme

--- nvim/.config/nvim/lua/chadrc.lua
+++ nvim/.config/nvim/lua/chadrc.lua
@@ -6,12 +6,7 @@
 local M = {}
 
 M.base46 = {
-	theme = "onedark",
-
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+	theme = "catppuccin",
 }
 
 return M
